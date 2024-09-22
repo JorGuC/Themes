f359f34be6b9989a6e3c2c68575dc9fa42a4818b
Jorge Guzmán <s21120196@alumnos.itsur.edu.mx>
2024-09-22 00:19:46 -0600

    Edicion de boton, tarjeta, barra de busqueda y fondo de color

--- app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -23,10 +23,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.reply.data.LocalEmailsDataProvider
 import com.example.reply.ui.theme.AppTheme
@@ -43,16 +45,19 @@ class MainActivity : ComponentActivity() {
             val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
             AppTheme {
-                ReplyApp(
-                    replyHomeUIState = uiState,
-                    closeDetailScreen = {
-                        viewModel.closeDetailScreen()
-                    },
-                    navigateToDetail = { emailId ->
-                        viewModel.setSelectedEmail(emailId)
-                    }
-                )
+                Surface(tonalElevation = 5.dp) {
+                    ReplyApp(
+                        replyHomeUIState = uiState,
+                        closeDetailScreen = {
+                            viewModel.closeDetailScreen()
+                        },
+                        navigateToDetail = { emailId ->
+                            viewModel.setSelectedEmail(emailId)
+                        }
+                    )
+                }
             }
+
 
         }
     }
